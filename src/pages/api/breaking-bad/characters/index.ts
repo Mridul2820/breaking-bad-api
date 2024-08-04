816641fb2a8b075fab637bd7d1cd62af0f88fa83
Mridul Panda <immridulpanda2820@gmail.com>
2024-08-05 00:43:09 +0530

    feat: Add API endpoints for Breaking Bad characters and quotes
    The code changes include the addition of API endpoints for retrieving Breaking Bad characters and quotes. These endpoints allow users to fetch specific characters or quotes by ID, retrieve a random character or quote, or get a list of all characters or quotes. This implementation uses the Supabase client for database operations.

---
+++ src/pages/api/breaking-bad/characters/index.ts
@@ -0,0 +1,46 @@
+import supabase from "@/client/supabase";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+  const { name, offset, limit } = req.query;
+
+  if (method === "GET") {
+    try {
+      if (name) {
+        let { data, error } = await supabase
+          .from("characters-breaking-bad")
+          .select("*")
+          .eq("name", name || "")
+          .range(
+            offset ? parseInt(offset as string) : 0,
+            limit ? parseInt(limit as string) : 10
+          );
+
+        if (error) {
+          res.status(400).json({ success: false, message: error.message });
+        }
+
+        res.status(200).json({ success: true, data: data });
+      } else {
+        let { data, error } = await supabase
+          .from("characters-breaking-bad")
+          .select("*")
+          .range(
+            offset ? parseInt(offset as string) : 0,
+            limit ? parseInt(limit as string) : 10
+          );
+
+        if (error) {
+          res.status(400).json({ success: false, message: error.message });
+        }
+
+        res.status(200).json({ success: true, data: data });
+      }
+    } catch (error: any) {
+      res.status(400).json({ success: false, message: error.message });
+    }
+  } else {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+};

9337558d4e4c04a258cf207f33f3f668513ad28c
Mridul Panda <immridulpanda2820@gmail.com>
2024-08-05 09:50:47 +0530

    feat: Add API endpoints for Breaking Bad episodes
    This commit adds API endpoints for retrieving Breaking Bad episodes. The endpoints allow users to fetch episodes by ID or season. The implementation uses the Supabase client for database operations.

--- src/pages/api/breaking-bad/episodes/index.ts
+++ src/pages/api/breaking-bad/episodes/index.ts
@@ -3,28 +3,30 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
-  const { author } = req.query;
+  const { season } = req.query;
 
   if (method === "GET") {
     try {
-      if (author) {
+      if (season) {
         let { data, error } = await supabase
-          .from("quotes-breaking-bad")
+          .from("episodes-breaking-bad")
           .select("*")
-          .eq("author", author);
+          .eq("season", season);
 
         if (error) {
           res.status(400).json({ success: false, message: error.message });
         }
+
         res.status(200).json({ success: true, data: data });
       } else {
         let { data, error } = await supabase
-          .from("quotes-breaking-bad")
+          .from("episodes-breaking-bad")
           .select("*");
 
         if (error) {
           res.status(400).json({ success: false, message: error.message });
         }
+
         res.status(200).json({ success: true, data: data });
       }
     } catch (error: any) {

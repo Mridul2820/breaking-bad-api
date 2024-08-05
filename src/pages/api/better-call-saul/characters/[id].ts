1cf1f6cdb5829937867565704e32a28f4f68bf11
Mridul Panda <immridulpanda2820@gmail.com>
2024-08-05 11:04:54 +0530

    feat: Add API endpoints for Better Call Saul deaths, quotes, episodes, and characters
    This commit adds API endpoints for retrieving Better Call Saul deaths, quotes, episodes, and characters. The endpoints allow users to fetch specific items by ID, retrieve a random item, or get a list of all items. The implementation uses the Supabase client for database operations.

---
+++ src/pages/api/better-call-saul/characters/[id].ts
@@ -0,0 +1,26 @@
+import supabase from "@/client/supabase";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+  const { id } = req.query;
+
+  if (method === "GET") {
+    try {
+      let { data, error } = await supabase
+        .from("characters-better-call-saul")
+        .select("*")
+        .eq("id", id);
+
+      if (error) {
+        res.status(400).json({ success: false, message: error.message });
+      }
+
+      res.status(200).json({ success: true, data: data![0] });
+    } catch (error: any) {
+      res.status(400).json({ success: false, message: error.message });
+    }
+  } else {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+};
---
+++ src/pages/api/better-call-saul/episodes/index.ts
@@ -0,0 +1,38 @@
+import supabase from "@/client/supabase";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+  const { season } = req.query;
+
+  if (method === "GET") {
+    try {
+      if (season) {
+        let { data, error } = await supabase
+          .from("episodes-better-call-saul")
+          .select("*")
+          .eq("season", season);
+
+        if (error) {
+          res.status(400).json({ success: false, message: error.message });
+        }
+
+        res.status(200).json({ success: true, data: data });
+      } else {
+        let { data, error } = await supabase
+          .from("episodes-better-call-saul")
+          .select("*");
+
+        if (error) {
+          res.status(400).json({ success: false, message: error.message });
+        }
+
+        res.status(200).json({ success: true, data: data });
+      }
+    } catch (error: any) {
+      res.status(400).json({ success: false, message: error.message });
+    }
+  } else {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+};
